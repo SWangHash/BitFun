e355a351dbe3c4cb8c9da01855f77789f9392756
Marqle <Marqle@noreply.gitcode.com>
2026-04-24 10:57:12 +0800

    vcoder base upload

---
+++ app/oh/Vcoder/entry/src/main/cpp/types/libbitfun_desktop_lib/Index.d.ts
@@ -0,0 +1,2 @@
+export declare function registerArktsFunction(funcName: string, callback: ((err: Error | null, arg: string) => Promise<string>)): void;
+export declare function setBuildResult(msg: string): void;
